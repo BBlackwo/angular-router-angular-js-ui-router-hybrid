a282667ae5a16d2d5921b1582fbf7d7bc700d7bc
Benjamin Blackwood <benjamin.blackwood@ratemyagent.com>
2018-08-28 15:12:35 +1000

    Part 2: Add UI Router With Config

--- src/app/legacy/legacy.app.module.ts
+++ src/app/legacy/legacy.app.module.ts
@@ -1,3 +1,10 @@
 import * as angular from 'angular';
+import angularUiRouter from 'angular-ui-router';
 
-export const legacyApp = angular.module('legacyApp', []);
+export const legacyApp = angular.module('legacyApp', [
+  angularUiRouter,
+])
+.config(['$locationProvider', ($locationProvider) => {
+  // Enable HTML 5 mode (so remove the default #/ prefix for routes)
+  $locationProvider.html5Mode({ enabled: true, requireBase: false });
+}]);
